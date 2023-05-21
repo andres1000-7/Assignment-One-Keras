d53732af34d574770a5f9f5aa4d9c8bee20fefc5
andres1000-7 <96263820+andres1000-7@users.noreply.github.com>
2023-05-21 02:31:53 -0400

    some testing

--- main.py
+++ main.py
@@ -6,5 +6,57 @@ As increase data, increase number of layers, performance should improve
 Read dataset in,
 
 No GPU, put in google code lab notebook, change runtime type (runs in less than 10 minutes)
+CHANGING DATASET SIZE 1st, CHANGE NN size 2nd
 """
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+tf.keras.datasets.cifar100.load_data(label_mode="fine")
+
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar100.load_data()
+
+# assert x_train.shape == (50000, 32, 32, 3)
+# assert x_test.shape == (10000, 32, 32, 3)
+# assert y_train.shape == (50000, 1)
+# assert y_test.shape == (10000, 1)
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+class_names = [
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle',
+    'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel',
+    'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock',
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster',
+    'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion',
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse',
+    'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear',
+    'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine',
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose',
+    'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail',
+    'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper',
+    'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train',
+    'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf',
+    'woman', 'worm'
+]
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    # The CIFAR labels happen to be arrays,
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
