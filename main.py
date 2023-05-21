cceda5124b5a6528a18e85bfab25980d370894d3
andres1000-7 <96263820+andres1000-7@users.noreply.github.com>
2023-05-21 14:34:49 -0400

    some testing

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-"""
-CNN, first one 3 layers, another one 4 layers, 5 layers, 6 layers
-Dataset size. 10 classes of 15 images each. total 150 images
-Increment, 15, 30, 45
-As increase data, increase number of layers, performance should improve
-Read dataset in,
-
-No GPU, put in google code lab notebook, change runtime type (runs in less than 10 minutes)
-CHANGING DATASET SIZE 1st, CHANGE NN size 2nd
-"""
-import os
+import numpy as np
+import matplotlib.pyplot as plt
 import keras
 from keras.datasets import cifar100
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from keras.utils import to_categorical
+from keras import backend as K
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+plt.rcParams['figure.figsize'] = (7,7) # Make the figures a bit bigger
+
+batch_size = 128
+num_classes = 100
+epochs = 3
 
 # Load the CIFAR-100 dataset
 (x_train, y_train), (x_test, y_test) = cifar100.load_data()
@@ -30,29 +25,33 @@ y_test = to_categorical(y_test, num_classes=100)
 
 # Define the model architecture
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+# Convolutional layers
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(32, 32, 3)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(100, activation='softmax'))
+model.add(Dense(num_classes, activation='softmax'))
 
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
 
 # Train the model
-model.fit(x_train, y_train, batch_size=128, epochs=20, validation_data=(x_test, y_test))
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
 
 # Evaluate the model
 score = model.evaluate(x_test, y_test, verbose=0)
+
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
